6002c78dbc94f8b95ced3dbdf5ced6f001307088
Miltonrmdf <miltonrmdf@gmail.com>
2025-05-25 21:30:36 -0300

    Exercício de Conversão Celsius para Fahrenheit

--- PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/Main.java
+++ PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Alô Mundo!!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
-    }
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano= true;
+        double notaDoFilme= 8.1;
+        //Média calculada por 3 pessoas
+        double media = (9.8+6.3+8.0)/3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de ação com galã do anos 80
+                Muito bom!!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println( sinopse);
+
+        int classificacao = (int) (media/2);
+
+        System.out.println("Classificação "+classificacao+ " Estrelas");
+
+
+        }
 }
