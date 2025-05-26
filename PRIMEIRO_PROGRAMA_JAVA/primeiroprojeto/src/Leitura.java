039c3743dddd27aa5fc1e53c78d23232a9f56da4
Miltonrmdf <miltonrmdf@gmail.com>
2025-05-26 01:31:53 -0300

    Aula 3 for

--- PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/Leitura.java
+++ PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/Leitura.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Leitura {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+
         System.out.println("Digite seu filme favorito");
         String filme = leitura.nextLine();
         System.out.println("Qual o ano de lançamento");
