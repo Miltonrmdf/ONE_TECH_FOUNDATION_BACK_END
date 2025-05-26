db0271f953b21bbb5df375162a3cb2d332fb0c06
Miltonrmdf <miltonrmdf@gmail.com>
2025-05-26 00:43:50 -0300

    Exercício 6

--- ExerciciosAula2/Desafio_HoraDaPraticaAula2/src/Main.java
+++ ExerciciosAula2/Desafio_HoraDaPraticaAula2/src/Main.java
@@ -76,5 +76,26 @@ Faça o casting da variável double para int e imprima o resultado.*/
                 valorEmDolares,valorEmReais,conversaoDolarEmReais);
         System.out.println(conversao);
 
+        String ex6= """
+                
+                Declare uma variável do tipo double precoOriginal. 
+                Atribua um valor em reais a essa variável, 
+                representando o preço original de um produto. 
+                Em seguida, declare uma variável do tipo double percentualDesconto
+                e atribua um valor percentual de desconto ao produto 
+                (por exemplo, 10 para 10%). 
+                Calcule o valor do desconto em reais, 
+                aplique-o ao preço original e imprima o novo preço com desconto.
+                """;
+        System.out.println(ex6);
+        double precoOriginal=258.63;
+        double percentualDesconto=15.0;
+        double valorDoDesconto=(percentualDesconto/100.0)*precoOriginal;
+        double precoComDescont=precoOriginal-valorDoDesconto;
+
+        System.out.println("O Preço original é R$"+precoOriginal);
+        System.out.println("O Percentual de desconto é R$" + percentualDesconto);
+        System.out.println("O preço com desconto é R$"+precoComDescont);
+
         }
 }
