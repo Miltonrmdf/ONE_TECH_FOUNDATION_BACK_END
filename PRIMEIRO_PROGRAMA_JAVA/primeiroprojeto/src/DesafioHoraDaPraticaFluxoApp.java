8af4336c3f227943ff86ea4184c423140ed7082f
Miltonrmdf <miltonrmdf@gmail.com>
2025-06-01 02:28:31 -0300

    03 Controlando o Fluxo da aplicação todos os exercícios no mesmo arquivo Desafio: hora da prática Ex2 finalizado

--- PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/DesafioHoraDaPraticaFluxoApp.java
+++ PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/DesafioHoraDaPraticaFluxoApp.java
@@ -60,9 +60,68 @@ public class DesafioHoraDaPraticaFluxoApp {
             }
     //--------------Final da Implementação ex1------------------------
 
-            }
-        }
+    //--------------Variáveis ex2---------------------------------
+        int numInteiro1,numInteiro2,numInteiroDigitado = 0;
+        Scanner leituraNumInteiroDigitado = new Scanner(System.in);
+    //--------------Final Variáveis ex2---------------------------
 
+    //--------------Variáveis ex2---------------------------
+        String ex2,textoNumIguais,textoNumDiferentes,
+                textoNumPrimeiroMaior,textoNumPrimeiroMenor,textoDigPriNum,textoDigSegNum;
+            ex2 = """
+            2.Peça ao usuário para inserir dois números inteiros.
+            Compare os números e imprima uma mensagem indicando
+            se são iguais, diferentes,
+            o primeiro é maior ou o segundo é maior.
+            """;
+            textoNumIguais= """
+                    Você digitou dois números iguais!""";
+            textoNumDiferentes= """
+                    Você digitou dois números diferentes!
+                    """;
+            textoNumPrimeiroMaior = """
+                    O primeiro número é maior!
+                    """;
+            textoNumPrimeiroMenor= """
+                    O primeiro número é menor
+                    """;
+            textoDigPriNum= """
+                    Digite o primeiro número:
+                    """;
+            textoDigSegNum= """
+                    Digite o segundo número:
+                    """;
+
+
+    //--------------Interação na tela ex2------------------------------
+        System.out.println(ex2);
+        System.out.println(textoDigPriNum);
+        numInteiro1=leituraNumInteiroDigitado.nextInt();
+        System.out.println(textoDigSegNum);
+        numInteiro2=leituraNumInteiroDigitado.nextInt();
+
+
+    //--------------Final Interação na tela ex2------------------------
+
+    //--------------Inicio implementação ex2------------------------
+        if (numInteiro2 == numInteiro1){
+            System.out.println(textoNumIguais);
+                }else{
+                    if(numInteiro1!=numInteiro2){
+                        System.out.println(textoNumDiferentes);
+                    }
+                }
+
+        if(numInteiro1>numInteiro2){
+            System.out.println(textoNumPrimeiroMaior);
+                }else{
+                    if (numInteiro1<numInteiro2){
+                        System.out.println(textoNumPrimeiroMenor);
+                        }
+                    }
+                }
+        }
+         //--------------Fim da implementação ex2------------------------
 
 
 
