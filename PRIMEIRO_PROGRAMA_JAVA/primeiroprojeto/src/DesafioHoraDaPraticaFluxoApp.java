0711f929d18f1e9110a914647ffd886d4febcfbb
Miltonrmdf <miltonrmdf@gmail.com>
2025-06-01 22:30:41 -0300

    03 Controlando o Fluxo da aplicação todos os exercícios no mesmo arquivo Desafio: hora da prática Ex4 finalizado

--- PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/DesafioHoraDaPraticaFluxoApp.java
+++ PRIMEIRO_PROGRAMA_JAVA/primeiroprojeto/src/DesafioHoraDaPraticaFluxoApp.java
@@ -193,6 +193,23 @@ public class DesafioHoraDaPraticaFluxoApp {
         }
     //--------------Final da resolução, raciocínio do ex3-----------------------------
 
+        //--------------Ex4-----------------------------
+
+        System.out.println("Crie um programa que solicite ao usuário um número e exiba a tabuada desse número de 1 a 10.");
+        Scanner taboadaDe = new Scanner(System.in);
+
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = taboadaDe.nextInt();
+
+        System.out.println("Tabuada do " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+ //--------------Final ex4-----------------------------
+
+
     }
 }
 
